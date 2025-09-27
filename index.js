72ca14e74422be00c26384d90509b48f01e0cb3b
sandarbhmeena-rgb <sandarbhmeena@gmail.com>
2025-09-27 23:23:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'YOUR_SERVER_IP', // without port (ex: play.aternos.me)
-  port: 25565, // Default Minecraft port
-  username: 'AFK_Bot' // Bot username
+  host: 'YOUR_SERVER_IP', // deathcraft25.aternos.me
+  port: 25565, // 53224
+  username: 'AFK_Bot' // AFK_Bot
 });
 
 bot.on('spawn', () => {
